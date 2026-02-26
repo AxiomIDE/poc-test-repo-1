13fa6b414be16a714394ca82927214c8f811bc47
ChristianGLucas <cgl.private@gmail.com>
2026-02-26 14:24:28 -0700

    included log statement

--- nodes/tokenize.go
+++ nodes/tokenize.go
@@ -11,6 +11,7 @@ import (
 // Tokenize splits input text on whitespace and returns the individual tokens
 // along with the total token count.
 func Tokenize(ctx context.Context, log axiom.Logger, input *gen.TextRequest) (*gen.TokensResult, error) {
+	log.Info("Hello, from Tokenize!")
 	tokens := strings.Fields(input.GetText())
 	return &gen.TokensResult{
 		Tokens: tokens,
